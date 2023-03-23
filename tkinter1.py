9d5e639b685b30e036bad834f33297f4752b867d
Kalle Niemi <kalleniemi@protonmail.com>
2023-03-23 12:59:53 +0200

    Changes to be committed: 	new file:   hellotkinter.py 	new file:   tkinter1.py

---
+++ tkinter1.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+'''
+TO DO
+
+Nappula mikä tallettaa textBoxista infon muuttujaan
+'''
+window = tk.Tk()
+window.title("Testi ikkuna")
+
+greeting = tk.Label(text= "Hello 123")
+greeting.pack()
+
+label = tk.Label(
+    text="Valkosta mustalla",
+    fg="white",
+    bg="black",
+    width=30,
+    height=5
+)
+label.pack()
+
+textBox = tk.Entry()
+textBox.pack(anchor="s")
+textBox.insert(0,"Kirjoita tähän")
+
+closeBtn = tk.Button(text="Sulje ikkuna", command=window.destroy)
+closeBtn.pack()
+
+
+
+root = tk.Tk()
+frm = tk.Frame(root)
+frm.grid()
+tk.Label(frm, text="Terveppä jooo!").grid(column=0, row=0)
+tk.Button(frm, text="Sulje ikkuna", command=root.destroy).grid(column=1, row=0)
+
+
+root.mainloop()
